cfb28d4792cc9b719f11737ac075c072a775b509
marcu <marcu.dragan@wolt.com>
2025-04-23 22:24:41 +0300

    add components

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -1,6 +1,9 @@
 import { Layout } from 'antd';
 import { Routes, Route } from "react-router-dom";
 import StudentPage from "./content/Student.jsx";
+import { Main } from './content/Main.jsx';
+import { Contacts } from './content/Contacts.jsx';
+import { About } from './content/About.jsx';
 
 const { Content } = Layout;
 
@@ -11,19 +14,16 @@ const contentStyle = {
     padding: '0 50px'
 };
 
-function MainPage() { return <h1>Main Page</h1>; }
-function AboutPage() { return <h1>About Page</h1>; }
-function ContactsPage() { return <h1>Contacts Page</h1>; }
 
 export default function ContentComponent() {
     return (
         <Content style={contentStyle}>
             <Routes>
-                <Route path="/" element={<MainPage />} />
-                <Route path="/main" element={<MainPage />} />
-                <Route path="/about" element={<AboutPage />} />
+                <Route path="/" element={<Main />} />
+                <Route path="/main" element={<Main />} />
+                <Route path="/about" element={<About />} />
                 <Route path="/student" element={<StudentPage />} />
-                <Route path="/contacts" element={<ContactsPage />} />
+                <Route path="/contacts" element={<Contacts />} />
             </Routes>
         </Content>
     );
